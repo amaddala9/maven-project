310262fcb55a11f8b9f897502f791aea0f013661
Ashok Maddala <ashok.maddala@us.ibm.com>
2018-06-27 11:25:48 -0700

    Some fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
   /**
-   * @param tag for 'someone'
+   * @param someone is the name
    * @return greeting string
    */
   public final String greet(final String someone) {
